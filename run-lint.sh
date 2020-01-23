39b3a34ab7ef4e0a581e281b4c115471e120a3ed
Shepel Serhii <serhii.shepel@agilelab.it>
2020-01-23 19:53:34 +0200

    Changed pylint requirement installation

--- run-lint.sh
+++ run-lint.sh
@@ -1,11 +1,1 @@
-function installPylint() {
-  pylint_path=$(which pylint)
-  echo $pylint_path
-  if [ -z "$pylint_path" ]
-  then
-        sudo apt-get install pylint
-  fi
-}
-
-installPylint
 pylint greetings numpy_greetings
