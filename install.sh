3fbd848c47ede099d7c2f80ad85a7335d5fe635b
Davide Icardi <davide.icardi@gmail.com>
2020-01-23 14:25:00 +0100

    first version with test, it, build

--- install.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-cd numpy_greetings
-pip install -r requirements.txt
-
-cd ..
---
+++ run-install.sh
@@ -0,0 +1,12 @@
+set -e
+
+function installRequirements() {
+  packageName=$1
+
+  echo "Install requirements for $packageName"
+
+  pip install -r ./$packageName/requirements.txt
+
+}
+
+installRequirements 'numpy_greetings'
