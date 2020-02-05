e9d1719262db35c46c96b0874168b4922d030ab2
Davide Icardi <davide.icardi@gmail.com>
2020-02-05 14:36:33 +0100

    Update run-test-it.sh

--- run-test-it.sh
+++ run-test-it.sh
@@ -1,7 +1,14 @@
 if [ "$VIRTUAL_ENV" = "" ]; then
     echo "Virtual environment not set"
-		exit 1
+    exit 1
+fi
+
+TEST_PARAM=$1
+TEST_COMMAND="discover -s test_it -t ."
+if [ "$TEST_PARAM" != "" ]; then
+  TEST_COMMAND="test_it.$TEST_PARAM"
 fi
 
 cd src || exit
-python -m unittest discover -s test-it -t .
+
+eval "python -m unittest $TEST_COMMAND"
